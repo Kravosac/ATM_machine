3c6282fa8bba13ba240ad35c23c7fd728765abb8
Fernando Kravosac <97983929+Kravosac@users.noreply.github.com>
2022-01-24 17:15:08 -0300

    Minor improvements

--- Functions.py
+++ Functions.py
@@ -1,12 +1,1 @@
-def enter(n):
-    try:
-        return int(n)
-    except ValueError:
-        return False
 
-
-def enter_str(n):
-    if n != 'y' or n != 'n':
-        return False
-    else:
-        return True
--- Withdraw.py
+++ Withdraw.py
@@ -1,21 +1,27 @@
-import Functions as f
-
 bills = {100: 10, 50: 10, 20: 10, 10: 10, 5: 10, 2: 10, 1: 10}  # The bills and its quantities
 possible_wd = sum([k * v for k, v in bills.items()])  # Possible withdraw amount (bill amount x qty)
 withdraws = []
+
+
+def enter(n):   # To avoid any other value than an intenger to be input
+    try:
+        return int(n)
+    except ValueError:
+        return False
+
 
 print("\nWelcome to the ATM machine. There are 10 bills of each amount available for withdraw.")
 
 while True:
 
     amount = input("\nPlease enter the withdraw amount (max $600): ")
-    while f.enter(amount) is False or int(amount) < 0 or int(amount) > 600:
+    while enter(amount) is False or int(amount) < 0 or int(amount) > 600:
         amount = input("\nPlease enter a valid intenger amount (max $600): ")
 
     if int(amount) > possible_wd:
         amount = input(f"\nSorry, not enough bills. Maximum amount available for withdraw "
                        f"is ${possible_wd}.00: ")
-        while f.enter(amount) is False or int(amount) < 0 or int(amount) > possible_wd:
+        while enter(amount) is False or int(amount) < 0 or int(amount) > possible_wd:
             amount = input(f"\nPlease enter a valid amount (max available is ${possible_wd}.00): ")
 
     withdraws.append(int(amount))
